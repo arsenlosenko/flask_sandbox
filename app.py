a0610cc9c11e81729540975acd823da6d4ae7ac0
Arsen Losenko <arsenlosenko@gmail.com>
2017-07-28 18:34:57 +0300

    Established socket connection

--- app.py
+++ app.py
@@ -7,22 +7,34 @@ To run this app do as follows:
 4. Run app by typing 'flask run' in the terminal
 """
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-from flask import Flask
-
-app = Flask(__name__)
 
 # TODO: setup socket connection
 # TODO: create default templates
 # TODO: connect to DB
 # TODO: setup tts
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_secured_password'
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def index():
-    return "This is index page"
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
     return "This is login page"
+
+
+@socketio.on('my event')
+def handle_my_event(json):
+    print("Recieved json: ", str(json))
+
+if __name__ == "__main__":
+    print("Starting app...")
+    socketio.run(app)
